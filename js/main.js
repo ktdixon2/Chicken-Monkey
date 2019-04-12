0aa0e8427d7a6fb67fd48a9a436df627733f3185
Ken Dixon <KDixon@Kens-Macbook-Air.local>
2019-04-12 13:36:13 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+console.log("Hello JS");
+
+
+for (var x=0; x<=100; x++) {
+    if (x === 0) {
+            console.log(x +  " is nothing");
+    }
+    else if (x % 5 === 0) {
+            console.log(x + " is absolutely a chicken");   
+    }
+    else if   (x % 7 ===0) {
+            console.log(x + " is possibly a monkey");
+    }
+}
